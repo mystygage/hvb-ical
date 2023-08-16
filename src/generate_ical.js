2c0e5e1d9fe163584f83a814e3f18e995539f3ae
mystygage <1107406+mystygage@users.noreply.github.com>
2023-08-16 11:14:38 +0200

    [Fix] Wintertime

--- src/generate_ical.js
+++ src/generate_ical.js
@@ -4,7 +4,8 @@ import { readJSONFromURL } from './extract_json.js';
 // Function to create a calendar entry from the JSON object.
 function createEventFromJSON(eventObj, cal) {
 
-  // Add time zone or set time to 0 o'clock, if all-day event
+  // Events are stored with a localized format (CET) without timezone information.
+  // The timezone is added and if all-day event the time is set to 0 o'clock.
   if(eventObj.start.includes('T')) {
     eventObj.start = eventObj.start + "+02:00"
     eventObj.end = eventObj.end + "+02:00"
@@ -13,8 +14,8 @@ function createEventFromJSON(eventObj, cal) {
     eventObj.end = eventObj.end + "T00:00+02:00"
   }
 
-  const startDateTime = new Date(eventObj.start);
-  const endDateTime = new Date(eventObj.end);
+  const startDateTime = convertToMESZ(new Date(eventObj.start));
+  const endDateTime = convertToMESZ(new Date(eventObj.end));
 
   // Distinguish between all-day events and time-limited events
   const allDay = startDateTime.getHours() === 0;
@@ -27,6 +28,27 @@ function createEventFromJSON(eventObj, cal) {
     url: eventObj.url,
     allDay: allDay,
   });
+}
+
+function getLastSundayOfMonth(year, month) {
+  const lastDayOfMonth = new Date(year, month + 1, 0);
+  const dayOfWeek = lastDayOfMonth.getDay();
+  return new Date(year, month, lastDayOfMonth.getDate() - dayOfWeek);
+}
+
+function convertToMESZ(date) {
+  const year = date.getFullYear();
+  const meszStart = getLastSundayOfMonth(year, 2); // Last Sunday in March
+  const meszEnd = getLastSundayOfMonth(year, 9);  // Last Sunday in October
+
+  if (date >= meszStart && date < meszEnd) {
+      // The date is in CEST, no correction required.
+      return date;
+  } else {
+      // The date is in winter time, add one hour
+      const correctedDate = new Date(date.getTime() + 3600000); // 3600000 ms = 1 hour
+      return correctedDate;
+  }
 }
 
 // Main function for creating the iCal calendar
